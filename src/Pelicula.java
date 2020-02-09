514fba038af85a62019484a4dc7b5641af972c79
danie <danie@DESKTOP-U4NK4R2>
2020-02-09 16:03:03 +0100

    he cambiado la linea 24

---
+++ src/Pelicula.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class Pelicula {
+	
+	private String titulo;
+	private String director;
+	private int duracion;
+	private Actor []actores = new Actor [15];
+	private int numeroActores;
+	
+	public Pelicula(){
+		this.titulo = "";
+		this.director = "";
+		this.duracion = 0;
+		this.actores= new Actor[15];
+		this.numeroActores = actores.length;
+	}
+	
+	public Pelicula(String titulo, String director, int duracion) {
+		super();
+		this.titulo = titulo;
+		this.director = director;
+		this.duracion = duracion;
+		this.actores = actores;
+		this.numeroActores = actores.length;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public int getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(int duracion) {
+		this.duracion = duracion;
+	}
+
+	public String toString() {
+		return "Pelicula [titulo=" + titulo + ", director=" + director + ", duracion=" + duracion + ", actores="
+				+ Arrays.toString(actores) + ", numeroActores=" + numeroActores + "]";
+	}
+	
+	
+	
+
+}
